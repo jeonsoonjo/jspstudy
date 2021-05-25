5c301a621f97dede51056485e8a305349bc9bdb4
jeonsoonjo <tinasoonj@naver.com>
2021-05-25 17:13:49 +0900

    0525-update

--- workspace/test/src/test/Test.java
+++ workspace/test/src/test/Test.java
@@ -1,5 +1,7 @@
 package test;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class Test {
@@ -7,7 +9,8 @@ public class Test {
 	public static void main(String[] args) {
 		
 		
-		/* String responseBody = "{\r\n" + 
+		/*
+		 	String responseBody = "{\r\n" + 
 				"  \"startDate\": \"2017-01-01\",\r\n" + 
 				"  \"endDate\": \"2017-04-30\",\r\n" + 
 				"  \"timeUnit\": \"month\",\r\n" + 
@@ -83,7 +86,23 @@ public class Test {
 				"		}";
 		
 		// 파싱 연습
-		JSONParser parser=new JSONParser();
+		JSONParser parser = new JSONParser();
+		JSONObject obj = null;
+		try {
+			obj = (JSONObject)parser.parse(responseBody);
+			String name = obj.get("name").toString();
+			long age = (long)obj.get("age");
+			JSONArray staff = (JSONArray)obj.get("staff"); // 배열이다
+			for(int i=0; i<staff.size(); i++) {
+				JSONObject obj2 = (JSONObject)staff.get(i);
+				String name2 = obj2.get("name").toString();
+				long age2 = (long)obj2.get("age");
+				System.out.println("이름: " + name2 + ", 나이: " + age2);
+			
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		
 		
